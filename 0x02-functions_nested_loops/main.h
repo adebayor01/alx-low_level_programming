b3fed0e25d88c9c72f2bcab8ee25b854b1b59d5d
adebayor01 <samuelbenedict234@gmail.com>
2022-09-14 08:36:25 -0700

    Write a function that adds two integers and returns the result.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,7 +11,7 @@ int _abs(int c);
 int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
-int add(int a, int b);
+int add(int n1, int n2);
 void print_to_98(int n);
 
 #endif
